812ec451c81c7378b41dd4a513ee6d6bf7f88d50
cneu4 <cedric.neumann@fh-bielefeld.de>
2021-12-01 13:40:52 +0100

    Initial commit

---
+++ src/firebase-config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCfsrWxrVv-j4Vc_LfcBPyVVwmMzopLdx0",
+  authDomain: "daytopat-add83.firebaseapp.com",
+  projectId: "daytopat-add83",
+  storageBucket: "daytopat-add83.appspot.com",
+  messagingSenderId: "446229225547",
+  appId: "1:446229225547:web:137f869dfbe5de13302dce"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
